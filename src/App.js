41d4743eecb1873342de03666a200b7494db591b
softdevmobe <softdevmobe@gmail.com>
2024-12-18 11:52:35 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,26 @@
-import React, { Component } from "react";
-import Users from "./components/users";
-import User from "./components/user";
-import Login from "./components/login";
-import Register from "./components/register";
-import Home from "./components/home";
-import NotFound from "./components/notFound";
-import ProtecedRoute from "./components/protectedRote";
-import Logout from "./components/logout";
-import Product from "./components/Product";
-import LoginForm from "./components/loginForm";
-import DashboardLayoutBasic from "./components/DashboardLayoutBasic"; // Import مسیر جدید
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DashboardContent from "./DashboardContent";
+import Login from "./Login";
+import DashboardLayoutBasic from "./DashboardLayoutBasic"; // داشبورد به‌عنوان لایه اصلی
+import ReportsContent from "./ReportsContent";
+import IntegrationsContent from "./IntegrationsContent";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-      
-        <DashboardLayoutBasic>
-        <div className="container mt-3">
-        
-        <Routes>
-          <Route path="/user/:userId" element={<User />} />
-          <Route path="/users" element={<Users />} />
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* اینجا DashboardLayoutBasic به‌عنوان لایه پایه قرار دارد */}
+        <Route path="/" element={<DashboardLayoutBasic />}>
+          <Route path="/dashboard" element={<DashboardContent />} />
+          <Route path="/orders" element={<DashboardContent />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/loginForm" element={<LoginForm />} />
-          
-          {/* مسیر جدید برای DashboardLayoutBasic */}
-          <Route path="/dashboard-layout" element={<DashboardLayoutBasic />} />
-
-          <Route
-            path="/"
-            element={
-              <ProtecedRoute>
-                <Home />
-              </ProtecedRoute>
-            }
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        </div>
-        </DashboardLayoutBasic>
-      
-      </>
-    );
-  }
+          <Route path="/reports" element={<ReportsContent />} />
+          <Route path="/integrations" element={<IntegrationsContent />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
