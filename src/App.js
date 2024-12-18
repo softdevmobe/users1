6ef0c0437ed8e5e86f83892836c32c7634608019
softdevmobe <softdevmobe@gmail.com>
2024-12-18 12:16:04 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DashboardContent from "./DashboardContent";
-import Login from "./Login";
-import DashboardLayoutBasic from "./DashboardLayoutBasic"; // داشبورد به‌عنوان لایه اصلی
+import Login from "./components/Login";
+import DashboardLayoutBasic from "./components/DashboardLayoutBasic"; // داشبورد به‌عنوان لایه اصلی
 import ReportsContent from "./ReportsContent";
-import IntegrationsContent from "./IntegrationsContent";
+
 
 function App() {
   return (
@@ -12,11 +12,11 @@ function App() {
       <Routes>
         {/* اینجا DashboardLayoutBasic به‌عنوان لایه پایه قرار دارد */}
         <Route path="/" element={<DashboardLayoutBasic />}>
-          <Route path="/dashboard" element={<DashboardContent />} />
-          <Route path="/orders" element={<DashboardContent />} />
+
+
           <Route path="/login" element={<Login />} />
-          <Route path="/reports" element={<ReportsContent />} />
-          <Route path="/integrations" element={<IntegrationsContent />} />
+
+
         </Route>
       </Routes>
     </Router>
