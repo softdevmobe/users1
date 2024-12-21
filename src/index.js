4251e944a3effe0d97c54dd6120c40101082331f
softdevmobe <softdevmobe@gmail.com>
2024-12-21 16:38:12 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import theme from './theme'; // تم سفارشی
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </BrowserRouter>
+
 );
