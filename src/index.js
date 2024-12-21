a3e39c50c3a31e39af953389ad57ab03f2fd822f
softdevmobe <softdevmobe@gmail.com>
2024-12-21 17:00:08 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // برای React 18
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme'; // تم سفارشی
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </BrowserRouter>
 );
