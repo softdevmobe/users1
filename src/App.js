8cfe78a6836a78ee69a6d3ec45f5abe17f11d63c
softdevmobe <softdevmobe@gmail.com>
2024-12-21 16:37:43 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
-import React, { Component } from "react";
-import Users from "./components/users";
-import User from "./components/user";
-import Login from "./components/login";
-import Register from "./components/register";
-import Home from "./components/home";
-import NotFound from "./components/notFound";
-import ProtecedRoute from "./components/protectedRote";
-import Logout from "./components/logout";
-import Product from "./components/Product";
-import LoginForm from "./components/loginForm";
-import DashboardLayoutBasic from "./components/DashboardLayoutBasic"; // مسیر اصلاح‌شده
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Routes>
-          {/* استفاده از Layout برای مسیرهای مشترک */}
-          <Route path="/" element={<DashboardLayoutBasic />}>
-            <Route index element={<Home />} />
-            <Route path="user/:userId" element={<User />} />
-            <Route path="users" element={<Users />} />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="logout" element={<Logout />} />
-            <Route path="product" element={<Product />} />
-            <Route path="loginForm" element={<LoginForm />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </Router>
-    );
-  }
+function Home() {
+  return <h2>Home Page</h2>;
+}
+
+function About() {
+  return <h2>About Page</h2>;
+}
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
