4d669fa8f0f436d70e8251adfdceb4bedca4671c
softdevmobe <softdevmobe@gmail.com>
2024-12-21 16:39:37 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 
-<<<<<<< HEAD
-class App extends Component {
-  render() {
-    return (
-
-        <Routes>
-          {/* استفاده از Layout برای مسیرهای مشترک */}
-          <Route path="/" element={<DashboardLayoutBasic />}>
-            <Route index element={<Home />} />
-            <Route path="user/:userId" element={<User />} />
-            <Route path="users" element={<Users />} />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="logout" element={<Logout />} />
-            <Route path="product" element={<Product />} />
-            <Route path="loginForm" element={<LoginForm />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-
-    );
-  }
-=======
 function Home() {
   return <h2>Home Page</h2>;
 }
@@ -44,7 +21,6 @@ function App() {
       </Routes>
     </div>
   );
->>>>>>> 8cfe78a6836a78ee69a6d3ec45f5abe17f11d63c
 }
 
 export default App;
