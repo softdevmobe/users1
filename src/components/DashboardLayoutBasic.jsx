af37215df08191e487b4bf01242d49e96b86213e
softdevmobe <softdevmobe@gmail.com>
2024-12-22 10:40:12 +0330

    Update DashboardLayoutBasic.jsx

--- src/components/DashboardLayoutBasic.jsx
+++ src/components/DashboardLayoutBasic.jsx
@@ -1,16 +1,10 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import Box from "@mui/material/Box";
-
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import BarChartIcon from "@mui/icons-material/BarChart";
-import DescriptionIcon from "@mui/icons-material/Description";
 import { AppProvider } from "@toolpad/core/AppProvider";
 import { DashboardLayout } from "@toolpad/core/DashboardLayout";
 import { useDemoRouter } from "@toolpad/core/internal";
-import { Link as RouterLink } from "react-router-dom";
-import { Link, Typography } from "@mui/material";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+
 
 const NAVIGATION = [
   {
@@ -20,66 +14,30 @@ const NAVIGATION = [
   {
     segment: "dashboard",
     title: "Dashboard",
-    icon: <DashboardIcon />,
+
   },
   {
     segment: "orders",
     title: "Orders",
-    icon: <ShoppingCartIcon />,
   },
   {
     segment: "login",
     title: "login",
-    icon: <ShoppingCartIcon />,
+
   },
-  
-      {
-        segment: "sales",
-        title: "Sales",
-        icon: <DescriptionIcon />,
-      },
-      {
-        segment: "traffic",
-        title: "Traffic",
-        icon: <DescriptionIcon />,
-      },
 ];
-
-
-function DemoPageContent({ pathname }) {
-  return (
-    <Box
-      sx={{
-        py: 4,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        textAlign: "center",
-      }}
-    >
-      <Typography>
-        <Link component={RouterLink} to={pathname}></Link> {pathname}{" "}
-      </Typography>
-    </Box>
-  );
-}
-
-DemoPageContent.propTypes = {
-  pathname: PropTypes.string.isRequired,
-};
 
 function DashboardLayoutBasic(props) {
   const { window } = props;
-  
 
   const router = useDemoRouter("/dashboard");
   console.log(router);
   const demoWindow = window !== undefined ? window() : undefined;
-
+  const navi = useNavigate();
   return (
     <AppProvider navigation={NAVIGATION} router={router} window={demoWindow}>
       <DashboardLayout>
-        <DemoPageContent pathname={router.pathname} />
+        navi({router.pathname})
       </DashboardLayout>
     </AppProvider>
   );
@@ -88,5 +46,4 @@ function DashboardLayoutBasic(props) {
 DashboardLayoutBasic.propTypes = {
   window: PropTypes.func,
 };
-
 export default DashboardLayoutBasic;
