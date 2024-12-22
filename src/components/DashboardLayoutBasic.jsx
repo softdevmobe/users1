56e21d16b8b48516bfcef8554d97ae463417bac8
softdevmobe <softdevmobe@gmail.com>
2024-12-22 09:56:03 +0330

    Update DashboardLayoutBasic.jsx
    داشبورد ساده

--- src/components/DashboardLayoutBasic.jsx
+++ src/components/DashboardLayoutBasic.jsx
@@ -2,12 +2,10 @@ import * as React from "react";
 import PropTypes from "prop-types";
 import Box from "@mui/material/Box";
 
-import { createTheme } from "@mui/material/styles";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import BarChartIcon from "@mui/icons-material/BarChart";
 import DescriptionIcon from "@mui/icons-material/Description";
-import LayersIcon from "@mui/icons-material/Layers";
 import { AppProvider } from "@toolpad/core/AppProvider";
 import { DashboardLayout } from "@toolpad/core/DashboardLayout";
 import { useDemoRouter } from "@toolpad/core/internal";
@@ -34,18 +32,7 @@ const NAVIGATION = [
     title: "login",
     icon: <ShoppingCartIcon />,
   },
-  {
-    kind: "divider",
-  },
-  {
-    kind: "header",
-    title: "Analytics",
-  },
-  {
-    segment: "reports",
-    title: "Reports",
-    icon: <BarChartIcon />,
-    children: [
+  
       {
         segment: "sales",
         title: "Sales",
@@ -56,30 +43,8 @@ const NAVIGATION = [
         title: "Traffic",
         icon: <DescriptionIcon />,
       },
-    ],
-  },
-  {
-    segment: "integrations",
-    title: "Integrations",
-    icon: <LayersIcon />,
-  },
 ];
 
-const demoTheme = createTheme({
-  cssVariables: {
-    colorSchemeSelector: "data-toolpad-color-scheme",
-  },
-  colorSchemes: { light: true, dark: true },
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 600,
-      lg: 1200,
-      xl: 1536,
-    },
-  },
-});
 
 function DemoPageContent({ pathname }) {
   return (
@@ -109,26 +74,18 @@ function DashboardLayoutBasic(props) {
 
   const router = useDemoRouter("/dashboard");
   console.log(router);
-
-  // Remove this const when copying and pasting into your project.
   const demoWindow = window !== undefined ? window() : undefined;
 
   return (
-    // preview-start
-    <AppProvider navigation={NAVIGATION} router={router} theme={demoTheme} window={demoWindow}>
+    <AppProvider navigation={NAVIGATION} router={router} window={demoWindow}>
       <DashboardLayout>
         <DemoPageContent pathname={router.pathname} />
       </DashboardLayout>
     </AppProvider>
-    // preview-end
   );
 }
 
 DashboardLayoutBasic.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * Remove this when copying and pasting into your project.
-   */
   window: PropTypes.func,
 };
 
