ee3a4c915190b3a6dd0cf39cc72570e0f9f24198
softdevmobe <softdevmobe@gmail.com>
2024-12-31 11:53:03 +0330

    a

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,7 +1,11 @@
 import axios from "axios";
 import React, { Component } from "react";
 import Input from "./input";
-import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+
 class Login extends Component {
   state = {
     account: {
@@ -11,40 +15,36 @@ class Login extends Component {
   };
   handleSubmit = async (e) => {
     e.preventDefault();
+    console.log("ok submit");
     try {
       const response = await axios.post("/api/users/login", this.state.account);
       const token = response.headers["authorization"];
       localStorage.setItem("token", token);
-      
     } catch (error) {
       alert("login failed .");
     }
   };
   handleChange = async ({ currentTarget: input }) => {
-    const account = { ...this.state.account,
-    [input.name] : input.value};
+    const account = { ...this.state.account, [input.name]: input.value };
     this.setState({ account });
   };
+
   render() {
     const { email, password } = this.state.account;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <Input
-          name="email"
-          value={email}
-          label="نام کاربری"
-          onChange={this.handleChange}
-        />
-        <Input
-          name="password"
-          value={password}
-          label="کلمه عبور"
-          onChange={this.handleChange}
-        />
-        <button className="btn btn-primary">Login</button>
+   
+         <Box sx={{ display:"flex", flexDirection:"column", padding:"20px" ,margin:"20px",border:"1px solid gray", minHeight: '100vh' ,alignItems:"center",justifyContent:"center" }}>
 
-      </form>
-      
+        <form onSubmit={this.handleSubmit}>
+          <Input name="email" value={email} label="نام کاربری" onChange={this.handleChange} />
+          <Input name="password" value={password} label="کلمه عبور" onChange={this.handleChange} />
+          <Button type="submit" variant="outlined">
+            Login
+          </Button>
+        </form>
+        </Box>
+        
+
     );
   }
 }
