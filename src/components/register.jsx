790a405d21cbdbf3dd11f9317edac53b251ae10f
beigi net <beiginet@sahand.local>
2025-01-22 16:16:19 +0330

    a

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,20 +1,48 @@
 import axios from "axios";
 import React, { Component, useState } from "react";
 import { TextField, Button, Box, Typography } from "@mui/material";
+import * as yup from "yup";
 const Register = () => {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  const [name_, setName_] = useState("");
-  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-
-
+  const [errors, setErrors] = useState({});
+  const [formData, setFormData] = useState({
+    userName: "",
+    password: "",
+    image: "",
+  });
+  const schema = yup.object().shape({
+    userName: yup
+      .string()
+      .required("نام کاریری نمی تواند خالی باشد ")
+      .min(3, "نام کاربری کوچکتر از 3 نباشد")
+      .max(10, "نام کاربری بزرگتر از 10 نباشد"),
+    password: yup
+      .string()
+      .required("کلمه عبور نمی تواند خالی باشد ")
+      .min(3, "کلمه عبور کوچکتر از 3 نباشد")
+      .max(10, "کلمه عبور بزرگتر از 10 نباشد"),
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!userName || !password || !name_) {
-      setError("نام کاربری و کلمه عبور را وارد کنید");
-      return;
+    try {
+      await schema.validate(formData, { abortEarly: false });
+      setErrors({});
+    } catch (err) {
+      if (err.name === "ValidationError") {
+        const errorMessages = {};
+        err.inner.forEach((error) => {
+          errorMessages[error.path] = error.message;
+        });
+        setErrors(errorMessages);
+        console.log(errors);
+      }
     }
+    return;
+
     axios.post("/api/users/register", this.state.user);
   };
 
@@ -33,30 +61,31 @@ const Register = () => {
       }}
     >
       <Typography variant="h5" gutterBottom>
-        اضافه کردن کاربر
+        {Object.keys(errors).map((key) => {
+          return <p style={{ color: "red" }}>{errors[key]}</p>;
+        })}
       </Typography>
       <form onSubmit={handleSubmit}>
         <TextField
           label="نام کاربری"
+          name="userName"
           type="userName"
           fullWidth
           margin="normal"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
+          value={formData.userName}
+          onChange={handleChange}
         />
+
         <TextField
           label="رمز عبور"
           type="password"
+          name="password"
           fullWidth
           margin="normal"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          value={formData.password}
+          onChange={handleChange}
         />
-<input 
-accept=".xlsx, .xls"
-id="file"
-type="file"
-/>
+        <input accept=".xlsx, .xls" id="file" type="file" />
         <Button
           type="submit"
           variant="contained"
